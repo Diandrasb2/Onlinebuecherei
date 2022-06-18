a0ee8fb4cc4527ab7e91f981de7c9c53030aab38
Diandra <92119418+Diandrasb2@users.noreply.github.com>
2022-06-18 11:01:34 +0200

    Code "Verschönerung" (strg i)

--- BüchereiProjekt/src/pojo/Buch.java
+++ BüchereiProjekt/src/pojo/Buch.java
@@ -1,99 +1,78 @@
 package pojo;
 
 public class Buch{
-String titel;
-String genre;
-String verfasser;
-Integer jahr;
-String verlag;
-Long isbn;
-String beschreibung;
 
+	String titel;
+	String genre;
+	String verfasser;
+	Integer jahr;
+	String verlag;
+	Long isbn;
+	String beschreibung;
 
+	public Buch(String titel, String genre, String verfasser, Integer jahr, String verlag, Long isbn, String beschreibung) {
+		this.titel=titel;
+		this.genre=genre;
+		this.verfasser=verfasser;
+		this.jahr=jahr;
+		this.verlag=verlag;
+		this.isbn=isbn;
+		this.beschreibung=beschreibung;
+	}
 
-public Buch(String titel, String genre, String verfasser, Integer jahr, String verlag, Long isbn, String beschreibung) {
-	this.titel=titel;
-	this.genre=genre;
-	this.verfasser=verfasser;
-	this.jahr=jahr;
-	this.verlag=verlag;
-	this.isbn=isbn;
-	this.beschreibung=beschreibung;
-	
-	
+	public String getBeschreibung() {
+		return beschreibung;
+	}
 
-}
+	public void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
 
+	public String getTitel() {
+		return titel;
+	}
 
+	public String getGenre() {
+		return genre;
+	}
 
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
 
-public String getBeschreibung() {
-	return beschreibung;
-}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
 
+	public void setVerfasser(String verfasser) {
+		this.verfasser = verfasser;
+	}
 
+	public void setJahr(Integer jahr) {
+		this.jahr = jahr;
+	}
 
+	public void setVerlag(String verlag) {
+		this.verlag = verlag;
+	}
 
-public void setBeschreibung(String beschreibung) {
-	this.beschreibung = beschreibung;
-}
+	public void setIsbn(Long isbn) {
+		this.isbn = isbn;
+	}
 
+	public String getVerfasser() {
+		return verfasser;
+	}
 
+	public Integer getJahr() {
+		return jahr;
+	}
 
+	public String getVerlag() {
+		return verlag;
+	}
 
-
-
-
-
-
-public String getTitel() {
-	return titel;
-}
-
-public String getGenre() {
-	return genre;
-}
-
-public void setTitel(String titel) {
-	this.titel = titel;
-}
-
-public void setGenre(String genre) {
-	this.genre = genre;
-}
-
-public void setVerfasser(String verfasser) {
-	this.verfasser = verfasser;
-}
-
-public void setJahr(Integer jahr) {
-	this.jahr = jahr;
-}
-
-public void setVerlag(String verlag) {
-	this.verlag = verlag;
-}
-
-public void setIsbn(Long isbn) {
-	this.isbn = isbn;
-}
-
-public String getVerfasser() {
-	return verfasser;
-}
-
-public Integer getJahr() {
-	return jahr;
-}
-
-public String getVerlag() {
-	return verlag;
-}
-
-public Long getIsbn() {
-	return isbn;
-}
-
-
-
+	public Long getIsbn() {
+		return isbn;
+	}
 }
--- BüchereiProjekt/src/pojo/Nutzer.java
+++ BüchereiProjekt/src/pojo/Nutzer.java
@@ -2,38 +2,39 @@ package pojo;
 
 public class Nutzer {
 
-String username;
-String email;
-String passwort;
+	String username;
+	String email;
+	String passwort;
 
-public Nutzer( String username, String email, String passwort) {
-	
-	this.username=username;
-	this.email=email;
-	this.passwort=passwort;
-	
-}
+	public Nutzer( String username, String email, String passwort) {
+		this.username=username;
+		this.email=email;
+		this.passwort=passwort;
+	}
 
+	public String getUsername() {
+		return username;
+	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-public String getUsername() {
-	return username;
-}
-public void setUsername(String username) {
-	this.username = username;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getPasswort() {
-	return passwort;
-}
-public void setPasswort(String passwort) {
-	this.passwort = passwort;
-}
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswort() {
+		return passwort;
+
+	}
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
+	}
 
 
 }
